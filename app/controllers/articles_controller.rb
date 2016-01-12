ee5a89694a9fa0ffa517b0c45cbbb9a0a43a7afc
sonu <sonu@amuratech.com>
2016-01-12 18:37:44 +0530

    install bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,44 @@
 class ArticlesController < ApplicationController
 
+	def index
+		@articles=Article.all
+	end
 	def new
 		@article = Article.new
 	end
 	def create
 		#render plain: params[:article].inspect
 		@article=Article.new(article_params)
-		@article.save
-		#redirect_to_articles_show(@article)
+		if @article.save
+			flash[:notice]="article created successfully"
 		redirect_to article_path(@article)
-
+		else
+			render 'new'
+		end
 	end
+	def show
+		@article=Article.find(params[:id])
+		
+	end
+	def edit
+		@article=Article.find(params[:id])
+	end
+	def update
+		@article=Article.find(params[:id])
+		if@article.update(article_params)
+			flash[:notice]="artical updated suceesfully"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+def destroy
+@article = Article.find(params[:id])
+@article.destroy
+flash[:notice] = "Article was successfully deleted"
+redirect_to articles_path
+end
+
 	private
 		def article_params
 			params.require(:article).permit(:title , :description)
